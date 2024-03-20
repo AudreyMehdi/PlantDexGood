d113f118445577ed1254838a52b4f93cf0ab53c2
AudreyMehdi <mehdi.audrey65@gmail.com>
2024-03-20 14:24:03 +0100

    first commit

--- src/app/services/plants.service.ts
+++ src/app/services/plants.service.ts
@@ -10,23 +10,23 @@ export class PlantsService {
   constructor(private http: HttpClient) {}
 
   getPlants(): Observable<Plant[]> {
-    return this.http.get<Plant[]>('http://localhost:3000/plants');
+    return this.http.get<Plant[]>('http://localhost:8080/plante');
   }
 
   createPlant(plantToCreate: Plant): Observable<Plant> {
-    return this.http.post<Plant>('http://localhost:3000/plants', plantToCreate);
+    return this.http.post<Plant>('http://localhost:8080/plante', plantToCreate);
   }
 
   deletePlant(planteToDeleteId: number){
-   return this.http.delete(`http://localhost:3000/plants/${planteToDeleteId}`);
+   return this.http.delete(`http://localhost:8080/plante/${planteToDeleteId}`);
    }
 
    getPlantById(plantId: number): Observable<Plant>{
-    return this.http.get<Plant>(`http://localhost:3000/plants/${plantId}`);
+    return this.http.get<Plant>(`http://localhost:8080/plante/${plantId}`);
    }
 
    updatePlant(plantId: number, plantModif: Plant): Observable<Plant>{
-    return this.http.put<Plant>(`http://localhost:3000/plants/${plantId}`, plantModif);
+    return this.http.put<Plant>(`http://localhost:8080/plante/${plantId}`, plantModif);
     
    }
 
